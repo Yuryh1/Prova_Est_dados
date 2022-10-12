968b151122366d18f692e5247862731c1cea4623
Yury Samuel <Yuryh084@gmail.com>
2022-10-11 21:21:39 -0300

    Add files via upload

--- prova/src/Q2/QUESTA2.java
+++ prova/src/Q2/QUESTA2.java
@@ -1,48 +1,53 @@
 package Q2;
-
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 import java.io.File;
+
+import java.io.FileNotFoundException;
+
 public class QUESTA2 {
     public static void main(String[] args) throws FileNotFoundException {
 
-        Queue<String> fileira01 = new LinkedList<>();
-        Queue<String> fileira02 = new LinkedList<>();
-        Queue<String> fileira03 = new LinkedList<>();
+        public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner scan = new Scanner(new File("./tools/deputadosfederais.txt"));
+            Queue<String> fileira01 = new LinkedList<>();
+            Queue<String> fileira02 = new LinkedList<>();
+            Queue<String> fileira03 = new LinkedList<>();
 
-        while(scan.hasNextLine()){
+            Scanner scan = new Scanner(new File("./tools/deputadosfederais.txt"));
 
-            if (fileira01.size() < 20){
-                fileira01.add(scan.nextLine());
-            } else if (fileira02.size() < 20) {
-                fileira02.add(scan.nextLine());
-            } else if (fileira03.size() < 20) {
-                fileira03.add(scan.nextLine());
-            }else{
-                System.out.println("   ");
+            while(scan.hasNextLine()){
+
+                if (fileira01.size() < 20){
+                    fileira01.add(scan.nextLine());
+                } else if (fileira02.size() < 20) {
+                    fileira02.add(scan.nextLine());
+                } else if (fileira03.size() < 20) {
+                    fileira03.add(scan.nextLine());
+                }else{
+                    System.out.println("CADEIRAS INSUFICIENTES!!!!!!!!!!");
+                }
+
+            }
+
+
+            System.out.println("Fileira 01: " + fileira01.size());
+            for (String nome:fileira01){
+                System.out.println(nome);
+            }
+
+            System.out.println("\n\n\nFileira 02: " + fileira02.size());
+            for (String nome:fileira02){
+                System.out.println(nome);
+            }
+
+            System.out.println("\n\n\nFileira 03: " + fileira03.size());
+            for (String nome:fileira03){
+                System.out.println(nome);
             }
 
         }
-
-
-        System.out.println("  " + fileira01.size());
-        for (String nome:fileira01){
-            System.out.println(nome);
-        }
-
-        System.out.println("  " + fileira02.size());
-        for (String nome:fileira02){
-            System.out.println(nome);
-        }
-
-        System.out.println("  " + fileira03.size());
-        for (String nome:fileira03){
-            System.out.println(nome);
-        }
-
-    }
+}
 }
